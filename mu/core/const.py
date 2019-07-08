45270b28d44df088d4feac78904f2f8a5f1282d1
faserg1 <faserg11@gmail.com>
2019-07-08 17:16:01 +0300

    Some cool files...

---
+++ mu/core/const.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+class EntityLogModifyType:
+	CREATE = 0
+	MODIFY = 1
+	DELETE = 2
--- mu/db/scheme/entity_log.py
+++ mu/db/scheme/entity_log.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
-from peewee import UUIDField, CharField
+from peewee import UUIDField, SmallIntegerField, ForeignKeyField, DateTimeField, TextField
+from .model import Model
+from .user import User
+from .org import Organization
+
 class EntityLog(Model):
     id = UUIDField(primary_key = True, help_text = '')
+	entity = UUIDField(index = True, help_text = '')
+	user = ForeignKeyField(User, help_text = '')
+	org = ForeignKeyField(Organization, null = True, help_text = '')
+	mod_type = SmallIntegerField(index = True, help_text = '')
+	timestamp = DateTimeField(index = True, help_text = '')
+	data = TextField(help_text = '')
+	user_msg = TextField(help_text = '')
 
     class Meta:
         table_name = 'entity_log'
