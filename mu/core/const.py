524295e09f599805688e45f701ee03f355d6352d
faserg1 <faserg11@gmail.com>
2019-07-11 09:49:52 +0300

    Const

--- mu/core/const.py
+++ mu/core/const.py
@@ -4,3 +4,13 @@ class EntityLogModifyType:
 	CREATE = 0
 	MODIFY = 1
 	DELETE = 2
+	
+class Privacy:
+	PUBLIC = 0
+	PROTECTED = 1
+	PRIVATE = 2
+	
+class ClusterNodeType:
+	SIBLING = 0
+	PARENT = 1
+	CHILD = 2
