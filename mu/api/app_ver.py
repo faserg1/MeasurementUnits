c960862e5df3bc736e3c179378472a3b70a5c4c5
faserg1 <faserg11@gmail.com>
2019-07-18 17:42:50 +0300

    Версия API

---
+++ mu/api/app_ver.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from .app_view import AppView
+
+class AppVer0_1(AppView):
+	
+	class Data:
+		route = "ver-0.1"
--- mu/api/app_view.py
+++ mu/api/app_view.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 
+import inspect
+from .app import app
+
 class AppView:
-	_base_route = "/"
-	_app = None
+	_full_route = "/"
+	_route = "/"
+	_parent = None
 	
-	def __init__(self, app, route, parent):
+	def __init__(self):
 		self._app = app
-		if parent:
-			_base_route = AppView.normalize_route(parent._base_route + "/" + route + "/")
+		data_cls = self._get_data_class()
+		if data_cls:
+			self._route = data_cls.route
+			self._parent = data_cls.parent
+		if self._parent:
+			_full_route = AppView._normalize_route(self._parent._full_route + "/" + self._route + "/")
 	
 	def route(self, func, *args, **kwargs):
-		self._app.add_url_rule(self._base_route, func.__name__, func, *args, **kwargs)
+		self._app.add_url_rule(self._full_route, func.__name__, func, *args, **kwargs)
+	
+	@classmethod
+	def _get_data_class(cls):
+		data = cls.__dict__['Data']
+		if inspect.isclass(data)
+			return data
 	
 	@staticmethod
-	def normalize_route(route):
+	def _normalize_route(route):
 		while route.count("//"):
 			route = route.replace("//", "/")
 		return route
