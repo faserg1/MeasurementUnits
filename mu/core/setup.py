741ba99bf7d5b21f3565eabf6836d2aa783ff305
faserg1 <faserg11@gmail.com>
2019-08-23 17:41:39 +0300

    WIP

--- mu/core/setup.py
+++ mu/core/setup.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
 
-from db.scheme.master import MasterDb
+from db.migration import Migration
 
 class Setup:
 	@staticmethod
 	def setup():
 		print("Setting up MeasurementUnits!")
-		if Setup.is_first_start():
-			Setup._first_start_setup()
-		Setup._updateDatabase()
-	
-	@staticmethod
-	def is_first_start():
-		return not MasterDb.table_exists() or not MasterDb.has_keys()
-	
-	@staticmethod
-	def _first_start_setup():
-		pass
-	
-	@staticmethod
-	def _updateDatabase():
-		print("Updating database...")
-		print(__func__.__name__ + ": stub")
+		Migration.updateDatabase()
--- mu/db/migration.py
+++ mu/db/migration.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+from db.scheme.Migration import Migration as MigrationTable
+
+class Migration:
+	@staticmethod
+	def updateDatabase():
+		if not MigrationTable.table_exists():
+			MigrationTable.create_table()
+		
+	def _fetchMigrationList():
+		pass
